98e5fa74e71ce0dd48e2389ef2483af7fa9596ba
Juan Sebastian Manzano Barbosa <64703666+JUSEMABA@users.noreply.github.com>
2022-03-15 22:05:04 -0500

    Add files via upload

---
+++ src/empresa/Principal.java
@@ -0,0 +1,12 @@
+package empresa;
+
+import empresa.vista.Menu;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menuPrincupal();
+    }
+    
+}
